12e8a3383add0fd3a67b5a4adb75bfce43b7b350
beyza <beyza.yakut@orthero.com.tr>
2024-03-19 14:34:14 +0300

    features added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import TaskCreate from './components/TaskCreate';
 import TaskList from './components/TaskList'; 
 
 function App() {
+  
+  const createTask=(title,taskDesc)=>{
+    console.log(title,taskDesc);
+  };
   return (
     <div className="App">
-      <TaskCreate />
+      <TaskCreate onCreate={createTask}/>
       <h1>Görevler</h1>
       <TaskList /> 
     </div>
--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,13 +1,31 @@
-function TaskCreate(){
+import {useState} from 'react';
+
+function TaskCreate({onCreate}){
+    const [title,setTitle]= useState('');
+    const [taskDesc,setTaskDesc]= useState('');
+
+    const handleChange=(event)=>{
+        setTitle(event.target.value);
+    };
+    const handleTaskChange=(event)=>{
+        setTaskDesc(event.target.value);
+    };
+    const handleSubmit=(event)=>{
+        event.preventDefault(); //bu sayede butona tıklandıgına ekran surekli refresh olmaz oylece kalır
+        onCreate(title,taskDesc);
+        setTitle(''); //text alanı ıcındekileri siler
+        setTaskDesc('');
+    }
+    
     return (
         <div className="task-create">
             <h3>Lütfen Task Giriniz!</h3>
             <form className="task-form">
             <label className="task-label">Başlık</label>
-            <input className="task-input"/>
+            <input value={title} onChange={handleChange} className="task-input"/>
             <label className="task-label">Task Giriniz</label>
-            <textarea className="task-input" rows={5} />
-            <button className="task-button">Oluştur</button>
+            <textarea value={taskDesc} onChange={handleTaskChange} className="task-input" rows={5} />
+            <button className="task-button" onClick={handleSubmit}>Oluştur</button>
             </form>
         </div>
     );
